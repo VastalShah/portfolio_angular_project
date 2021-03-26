803769270d506a3f0b322d5998e9de2430e2079e
Vatsal Shah <vashah@teksystems.com>
2021-03-27 00:02:04 +0530

    added form validation for signin

--- portfolio-app/src/app/signin/signin.component.ts
+++ portfolio-app/src/app/signin/signin.component.ts
@@ -11,14 +11,43 @@ export class SigninComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) { }
 
   userForm!: FormGroup;
-  formControls!: any;
+  formControls: any;
+  formSubmitted!: boolean;
+  pass: any;
+  email: any;
 
   ngOnInit(): void {
     this.userForm = this.formBuilder.group({
-      email: ['', Validators.required, Validators.email],
+      Email: ['', Validators.required, Validators.email],
       password: ['', Validators.required]
     })
     this.formControls = this.userForm.controls;
+  }
+
+  handleEmail(event: any){
+    this.email = event.target.value;
+  }
+
+  handlePassword(event: any){
+    this.pass = event.target.value;
+  }
+
+  handleFormData(){
+    this.formSubmitted = true;
+    if(this.userForm.invalid){
+      console.log("Form not submited");
+      return;
+    } else{
+      if(localStorage.length > 0){
+        if(localStorage.getItem("email") == this.email && localStorage.getItem("password") == this.pass){
+          alert("You have signed in!");
+        }else{
+          alert("Username or password do not match");
+        }
+      } else{
+        alert("You are not registered. Please signup")
+      }
+    }
   }
 
 }
