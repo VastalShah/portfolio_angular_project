13a4a01600261281fdabffab7f03c5a1b6d64ade
Vatsal Shah <vashah@teksystems.com>
2021-03-26 23:21:19 +0530

    added form validation for signup

--- portfolio-app/src/app/signup/signup.component.ts
+++ portfolio-app/src/app/signup/signup.component.ts
@@ -12,15 +12,51 @@ export class SignupComponent implements OnInit {
 
   userForm!: FormGroup;
   formControls: any;
+  formSubmitted!: boolean;
+  pass: any;
+  confirmpass: any;
+  email: any;
 
   ngOnInit(): void {
     this.userForm = this.formBuilder.group({
       UserName: ['', Validators.required],
-      Email: ['', Validators.required, Validators.email],
-      password: ['', Validators.required, Validators.minLength(5)],
-      confirmPassword: ['', Validators.required, Validators.minLength(5)]
+      Email:  ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
     })
     this.formControls = this.userForm.controls;
   }
 
+  handleFormData(){
+    this.formSubmitted = true;
+    if(this.userForm.invalid){
+      console.log("Form not submited");
+      return;
+    } else{
+      if(this.pass != this.confirmpass){
+        alert("UserName or password should be same");
+        return;
+      } else{
+        if(localStorage.getItem("email") == this.email && localStorage.getItem("password") == this.pass){
+          alert("You are already registered please signin");
+        } else{
+          localStorage.setItem("email", this.email);
+          localStorage.setItem("password", this.pass);
+        }
+      }
+    }
+  }
+
+  handleEmail(event: any){
+    console.log(event.target.value);
+    this.email = event.target.value;
+  }
+
+  handleConfirmPassword(event: any){
+    this.pass = event.target.value;
+  }
+
+  handlePassword(event: any){
+    this.confirmpass = event.target.value;
+  }
 }
